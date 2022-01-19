3cf44d86bfc7a430c0ca54e8d432427c5ba04821
DevXerxes <87164130+DevXerxes@users.noreply.github.com>
2022-01-18 22:16:38 -0800

    Updated ShippingQuote App
    Added else statements, using a structure of nesting statements as an algorithm.

--- ShippingQuoteApp/Program.cs
+++ ShippingQuoteApp/Program.cs
@@ -13,7 +13,7 @@ namespace ShippingQuoteApp
             //intro of program
             Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
 
-            //asking user for height of package
+            //asking user for weight of package
             Console.WriteLine("Please enter the package weight: ");
             //acquire inputted weight using an integer(string casted into int data type)
             //variable to hold that user input(value)
@@ -24,46 +24,40 @@ namespace ShippingQuoteApp
             {
                 Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
             }
-
-            //asking for package width, doing same process for weight prompt
-            Console.WriteLine("Please enter the package width:");
-            int pWidth = Convert.ToInt32(Console.ReadLine());
-            if (pWidth > 50)
+            else
             {
-                Console.WriteLine("Package too big to be shipped via Package Express. Have a good day.");
+                //asking for package width, doing same process for weight prompt
+                Console.WriteLine("Please enter the package width:");
+                int pWidth = Convert.ToInt32(Console.ReadLine());
+                //asking for package height, doing same process for weight prompt
+                Console.WriteLine("Please enter the package height:");
+                int pHeight = Convert.ToInt32(Console.ReadLine());
+                //asking for package length, doing same process for weight prompt
+                Console.WriteLine("Please enter the package width:");
+                int pLength = Convert.ToInt32(Console.ReadLine());
+                int sum = pWidth + pLength + pHeight;
+                if (sum > 50)
+                {
+                    Console.WriteLine("Package too big to be shipped via Package Express. Have a good day.");
+                }
+                else
+                {
+                    //multiplying the 3 dimesions together: height, width and length
+                    decimal triMult = pWidth * pHeight * pLength;
+                    //multplying product of triMult by the weight of package
+                    decimal product = triMult * pWeight;
+                    //dividing outcome of product operation to get quote of shipping costs
+                    decimal result = product / 100.00m;
+
+                    //convert result of quote math operations
+                    //to decimal form (20.00)
+                    int total = 1;
+                    decimal quote = result * total;
+                    //multiply end product by decimal data type (Ex: 1.00m)
+                    Console.WriteLine("Your estimated total for shipping this package is: $" + quote * 1.00m);
+                    Console.WriteLine("Thank you!");
+                }
             }
-
-            //asking for package height, doing same process for weight prompt
-            Console.WriteLine("Please enter the package height:");
-            int pHeight = Convert.ToInt32(Console.ReadLine());
-            if (pHeight > 50)
-            {
-                Console.WriteLine("Package too big to be shipped via Package Express. Have a good day.");
-            }
-
-            //asking for package length, doing same process for weight prompt
-            Console.WriteLine("Please enter the package width:");
-            int pLength = Convert.ToInt32(Console.ReadLine());
-            if (pLength > 50)
-            {
-                Console.WriteLine("Package too big to be shipped via Package Express. Have a good day.");
-            }
-
-            //multiplying the 3 dimesions together: height, width and length
-            decimal triMult = pWidth * pHeight * pLength;
-            //multplying product of triMult by the weight of package
-            decimal product = triMult * pWeight;
-            //dividing outcome of product operation to get quote of shipping costs
-            decimal result = product / 100.00m;
-
-            //convert (casting) result of quote math operations
-            //to decimal form (20.00)
-            int total = 1;
-            decimal quote = result * total;
-            //multiply end product by decimal data type (Ex: 1.00m)
-            Console.WriteLine("Your estimated total for shipping this package is: $" + quote * 1.00m);
-            Console.WriteLine("Thank you!");
-
 
             Console.ReadLine();
         }
