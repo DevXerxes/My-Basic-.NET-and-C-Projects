4717d81017c838b8f6cae79665b23683bf4c5577
DevXerxes <87164130+DevXerxes@users.noreply.github.com>
2022-01-19 19:08:09 -0800

    Complex String Functions app
    Includes use of:
        -Concatenates three strings.
        -Converts a string to uppercase
        -Creates a Stringbuilder and builds a paragraph of text, one sentence at a time

---
+++ StringAssignment/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //concatenating (adding words together) 3 strings
+            string one = "Hello, ";
+            string two = "my name is ";
+            string three = "Marlon.";
+            //using the "+" sign to concatenate the strings
+            //using ToUpper command to print the string attached to it
+            //as all uppercased letters
+            Console.WriteLine(one + two + three.ToUpper());
+
+            //using StringBuilder to allows a developer to 
+            //add new lines of code, in this case strings, to existing lines of code (strings)
+            StringBuilder aboutMe = new StringBuilder();
+
+            //using Append() to tell computer to add new lines
+            //of code to existing code
+            //used \n to add new strings on new line
+            aboutMe.Append("I am a Software Developer in the making." +
+                "\nChallenges are welcome by me, cause they stretch my strength." +
+                "\nI am ready to build more complex programs and softwares.");
+            Console.WriteLine(aboutMe);
+
+            Console.ReadLine();
+        }
+    }
+}
