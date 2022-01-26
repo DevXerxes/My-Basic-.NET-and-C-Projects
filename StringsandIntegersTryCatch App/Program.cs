bcd20f94e552393aeed13f632b5e663fa2b194d8
DevXerxes <87164130+DevXerxes@users.noreply.github.com>
2022-01-26 00:04:37 -0800

    Strings and Integers TryCatch App
    Program includes/contains:
        - a list of integers
        -Asking the user for a number to divide each number in the list by.
        -a loop that takes each integer in the list, divides it by the number the user entered, and displays the result to the screen.
        -print a message to the display to let you know the program has emerged from the try/catch block and continued on with program execution below and outside trycatch block, using the finally block
        -the proper error messages display on the screen, and that the code after the try/catch block gets executed.
        -using catch block to display proper error messages for valid numbers, zeros and strings

---
+++ StringsandIntegersTryCatch App/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //using try/catch block to catch any erros
+        //and display a friendly error message to users
+        try
+        {
+            // list of integers
+            List<int> divideNums = new List<int>()
+        {
+            64,
+            56,
+            112,
+            24,
+            82
+        };
+            //asking user for a number to use for
+            //dividing it by all the integers in the list
+            Console.WriteLine("Can you provide any number?");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+
+            //using foreach loop to each integer in list
+            //and divide it by number entered by user
+            foreach (int Num in divideNums)
+            {
+                //displaying each result on the console
+                Console.WriteLine("List number divided by your number equals: " + Num / userNum);
+            }
+            Console.ReadLine();
+        }
+        //catch block to let user know to enter 
+        //whole numbers, not strings(words)
+        catch (FormatException ex)
+        {
+            Console.WriteLine("Please type in a whole number.");
+            return;
+        }
+        //catch block to display error message 
+        //for using zeros to divide with
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine("Please don\'t divide by zero.");
+        }
+        //catch block to catch any error and display
+        //the proper error message according to the input
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        //finally block to always display the message below
+        finally
+        {
+            Console.WriteLine("Program has emerged from try/catch block and continued on with program execution.");
+            Console.ReadLine();
+        }
+        
+    }
+}
