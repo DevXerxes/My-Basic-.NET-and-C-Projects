364163770ff879bce8cc56d7acfba9b1a0a3d3bc
DevXerxes <87164130+DevXerxes@users.noreply.github.com>
2022-01-27 19:30:33 -0800

    Calling Math methods App
    A program that includes/uses:
        -a class in separate .cs file that has 3 different math operation methods
        -knowledge on using public an d private classes
        -running a user input to each method as a parameter

---
+++ CallingMethodsApp/MathMethod.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsApp
+{
+    public class MathMethod
+    {
+        //setting methods public to be accessible anywhere
+        //first method is adding integers
+        public int AddNum(int data, int value)
+        {
+            
+            return data + value;
+        }
+        //2nd method subtracting integers
+        public int SubNum(int data, int value)
+        {
+            
+            return data - value;
+        }
+        //3rd method multiplies integers
+        public int MultNum(int data, int value)
+        {
+            return data * value;
+        }
+    }
+}
---
+++ CallingMethodsApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace CallingMethodsApp
+{
+    class Program
+    {
+        public static int Data { get; set; }
+        static void Main(string[] args)
+        {
+            //asking user for a number to use our methods on
+            Console.WriteLine("What number would you like to use for a few math operations to be done with?");
+            var userNum = Convert.ToInt32(Console.ReadLine());
+
+            //referencing the object of user input
+            var operationObject = new MathMethod();
+            //calling the methods by saving it to result variable
+            var result = operationObject.AddNum(userNum, 7);
+            var result2 = operationObject.SubNum(userNum, 9);
+            var result3 = operationObject.MultNum(userNum, 12);
+            
+
+            //printing the returned value (result) to console
+            Console.WriteLine(result);
+            Console.WriteLine(result2);
+            Console.WriteLine(result3);
+            Console.ReadLine();
+        }
+    }
+}
