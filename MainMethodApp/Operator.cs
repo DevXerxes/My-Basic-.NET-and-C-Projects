6299a4d417a231f884579815c9621bfb6d5b9444
DevXerxes <87164130+DevXerxes@users.noreply.github.com>
2022-01-30 22:22:43 -0800

    Method Overloading app
    Includes use of three methods with same name, doing different math operations in each method.
        -Int to int parameters
        -decimal to int parameters
        -string to int parameters

---
+++ MainMethodApp/Operator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MainMethodApp
+{
+    class Operator
+    {
+        //a public method to be accessible anywhere on console app
+        // this method takes in an integer and adds it with another
+        //then returns result as an integer
+        public int NumOp(int data, int value)
+        {
+            return data + value;
+        }
+
+        //2nd method with same name but different math operation (decimal integer)
+        public decimal NumOp(decimal data, decimal value)
+        {
+            return data - value;
+        }
+
+        //3rd method with same name, taking in string and displaying
+        // result as an integer
+        public string NumOp (string data, int value)
+        {
+            return Int32.TryParse(data) * value;
+        }
+    }
+}
---
+++ MainMethodApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodApp
+{
+    class Program
+    {
+        public static int Data { get; set; }
+        static void Main(string[] args)
+        {
+            //entering a number to use for the method in class.cs
+            Console.WriteLine("Enter a number: ");
+            var entNum = Convert.ToInt32(Console.ReadLine());
+            decimal decNum = 12.5m;
+
+            //referencing the data of user input
+            //instantiating the class
+            var getValue = new Operator();
+            //calling the one method then display the integer to screen
+            var display = getValue.NumOp(entNum, 6);
+            var display2 = getValue.NumOp(decNum, 9.0m);
+            var display3 = getValue.NumOp(entNum, 12);
+
+            //printing returned value (result) to console
+            Console.WriteLine(display);
+            Console.WriteLine(display2);
+            Console.WriteLine(display3);
+            Console.ReadLine();
+        }
+    }
+}
