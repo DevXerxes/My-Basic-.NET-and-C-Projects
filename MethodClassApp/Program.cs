02a9ae9a503abcd7899b77f51d032b20ba4c9cd8
DevXerxes <87164130+DevXerxes@users.noreply.github.com>
2022-02-01 23:04:16 -0800

    Void Method App
    An app that uses the void method.

---
+++ MethodClassApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiating the class Void
+            Void veid = new Void();
+            veid.NumVoid(6, 7);
+            Convert.ToString(veid.NumVoid("six", "seven"));
+        }
+    }
+}
---
+++ MethodClassApp/Void.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassApp
+{
+    class Void
+    {
+        //creating void (returns no result) method with two integers as parameters
+        public void NumVoid(int a, int b)
+        {
+            //first num just takes a math operation, no display
+            int operation = a * b;
+            //2nd num displays given integer only
+            Console.WriteLine(b);
+        }
+    }
+}
