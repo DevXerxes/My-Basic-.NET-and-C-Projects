03606fb85113e44890d5293750b01e12212d7457
DevXerxes <87164130+DevXerxes@users.noreply.github.com>
2022-02-01 18:51:19 -0800

    Making a parameter optional App
    includes a method with two parameters, one being optional.

---
+++ MethodOptionApp/Option.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOptionApp
+{
+    class Option
+    {
+        //public class so its accessible anywhere and data type int=
+        // for asking what the result will be
+        public int NumOpt(int data, int option = 3)//gave variable "option" a set value so it's made an option
+        {
+            return data * option;
+        }
+    }
+}
---
+++ MethodOptionApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOptionApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiating the Option class
+            Option optshin = new Option();
+            //letting user know to enter 2 numbers, one at a time
+            //2nd entry is optional
+            Console.WriteLine("Please enter two numbers, " +
+                "\n Enter first one here: ");
+            var result = Convert.ToInt32(Console.ReadLine());
+            //here user enters 2nd number, which is optional
+            Console.WriteLine("Enter second number here, optional, can leave blank: ");
+            var result2 = Convert.ToInt32(Console.ReadLine());
+
+            //calling in method from class Option
+            var display = optshin.NumOpt(result, 6);
+            var display2 = optshin.NumOpt(result2);
+
+            Console.WriteLine("Your first number result is: " + display);
+            Console.WriteLine("Your second number result is: " + display2);
+            Console.ReadLine();
+        }
+    }
+}
