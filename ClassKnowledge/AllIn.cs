5b768b88afdf23a51b1495acc4f64b256b0fec2a
DevXerxes <87164130+DevXerxes@users.noreply.github.com>
2022-02-04 00:04:39 -0800

    Class Methods App
    Includes use of: 
        -Created a class. In that class, create a void method that outputs an integer. Have the method divide the data passed to it by 2
        -In the Main() method, instantiate that class
        -Have the user enter a number. Call the method on that number. Display the output to the screen. It should be the entered number, divided by two
        - method with output parameters
        -Overload a method
        -Declare a class to be static

---
+++ ClassKnowledge/AllIn.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassKnowledge
+{
+    class AllIn
+    {
+        //void method outputting an integer, dividing data passed to it by 2
+        public void OutVoid(int data)
+        {
+            Console.WriteLine(data / 2);
+        }
+
+        //creating a method with output parameters
+        public void getValues(out int m, out int n)
+        {
+            m = 22;
+            n = 24;
+        }
+        //overloading a method 
+        public void getValues(int h, int i)
+        {
+            h = 22;
+            i = 24;
+            Console.WriteLine(h * i);
+        }
+        //declaring a class to be static
+        public static class MathOp
+        {
+            public static int NumNums(int b, int a)
+            {
+                b = 9;
+                a = 8;
+                return b * a;
+            }
+        }
+    }
+}
---
+++ ClassKnowledge/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassKnowledge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate the class
+            AllIn todo = new AllIn();
+            //asking user to enter a number
+            Console.WriteLine("Please enter a number: ");
+            int userIn = Convert.ToInt32(Console.ReadLine());
+            //calling method on the user's number
+            todo.OutVoid(userIn);
+            //printing the output to the screen
+            Console.ReadLine();
+        }
+    }
+}
