26d6bd3ca952757e55ee8f8ce17b37b98cc51354
DevXerxes <87164130+DevXerxes@users.noreply.github.com>
2022-02-03 19:11:40 -0800

    Updated Method Option app
    added try/catch block for displaying proper error messages. custom made.

--- MethodOptionApp/Program.cs
+++ MethodOptionApp/Program.cs
@@ -10,23 +10,33 @@ namespace MethodOptionApp
     {
         static void Main(string[] args)
         {
-            //instantiating the Option class
-            Option optshin = new Option();
-            //letting user know to enter 2 numbers, one at a time
-            //2nd entry is optional
-            Console.WriteLine("Please enter two numbers, " +
-                "\n Enter first one here: ");
-            var result = Convert.ToInt32(Console.ReadLine());
-            //here user enters 2nd number, which is optional
-            Console.WriteLine("Enter second number here, optional, can leave blank: ");
-            var result2 = Convert.ToInt32(Console.ReadLine());
+            //using a try/catch block to provide a friendly
+            //error message of not entering an integer for the second number
+            try
+            {
+                //instantiating the Option class
+                Option optshin = new Option();
+                //letting user know to enter 2 numbers, one at a time
+                //2nd entry is optional
+                Console.WriteLine("Please enter two numbers, " +
+                    "\n Enter first one here: ");
+                var result = Convert.ToInt32(Console.ReadLine());
+                //here user enters 2nd number, which is optional
+                Console.WriteLine("Enter second number here, optional, can leave blank: ");
+                var result2 = Convert.ToInt32(Console.ReadLine());
 
-            //calling in method from class Option
-            var display = optshin.NumOpt(result, 6);
-            var display2 = optshin.NumOpt(result2);
+                //calling in method from class Option
+                var display = optshin.NumOpt(result, 6);
+                var display2 = optshin.NumOpt(result2);
 
-            Console.WriteLine("Your first number result is: " + display);
-            Console.WriteLine("Your second number result is: " + display2);
+                Console.WriteLine("Your first number result is: " + display);
+                Console.WriteLine("Your second number result is: " + display2);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("It\'s okay, no need to enter a second number.");
+            }
+            
             Console.ReadLine();
         }
     }
