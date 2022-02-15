51613ebf765569f6023aabf6ede3e41a0c6e6ac7
DevXerxes <87164130+DevXerxes@users.noreply.github.com>
2022-02-14 18:58:28 -0800

    LAMBDA App
    Publishing code that i have so far, needs fixing.

---
+++ LAMBDA/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAMBDA
+{
+    class Employee
+    {
+        public Employee(string v1, string v2, int v3)
+        {
+            firstName = v1;
+            lastName = v2;
+            Id = v3;
+        }
+
+        //two string properties
+        //made public to be accessible to any other class
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        //int method taking in Id for each new employee
+        public int Id { get; set; }
+    }
+}
---
+++ LAMBDA/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAMBDA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiating the employee class with person properties inherited
+            //Employee user = new Employee();
+            //a list with 10 employees, 2 with same first name
+            List<Employee> employeesList = new List<Employee>();
+            //Adding the 10 employees to the list using Add() method
+            employeesList.Add(new Employee("Joe", "Smith",1));
+            employeesList.Add(new Employee("Maria", "Garcia", 2));
+            employeesList.Add(new Employee("Kevin", "Ryder", 3));
+            employeesList.Add(new Employee("Joe", "Smith", 4));
+            employeesList.Add(new Employee("Bill", "Hunt", 5));
+            employeesList.Add(new Employee("Shawn", "Levi", 6));
+            employeesList.Add(new Employee("Jenny", "Bogner", 7));
+            employeesList.Add(new Employee("Chang", "Rue", 8));
+            employeesList.Add(new Employee("Joe", "Smith", 9));
+            employeesList.Add(new Employee("Tim", "Turner", 10));
+            employeesList.Add(new Employee("Nelly", "Fyiner", 11));
+            employeesList.Add(new Employee("Jesse", "Smith", 12));
+
+            //foreach loop to create new list of employees
+            //with first name "Joe"
+            foreach (Employee employee in employeesList)
+            {
+                if (employee.firstName == "Joe")
+                {
+                    Console.WriteLine(employee);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
