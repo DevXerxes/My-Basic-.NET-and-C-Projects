09a8ecdca65d2b0fabe8b909f4acdcc011919804
DevXerxes <87164130+DevXerxes@users.noreply.github.com>
2022-02-15 20:04:09 -0800

    LAMBDA expression App
    Includes:
        -In the Main() method,  a list of at least 10 employees. Each employee should have (3 properties) a first and last name, as well as an Id. At least two employees should have the first name “Joe”
        -a foreach loop, creating a new list of all employees with the first name “Joe”
        -Performed the same action again, but this time with a lambda expression
        -a lambda expression, with a list of all employees with an Id number greater than 5

--- LAMBDA/Program.cs
+++ LAMBDA/Program.cs
@@ -18,25 +18,55 @@ namespace LAMBDA
             employeesList.Add(new Employee("Joe", "Smith",1));
             employeesList.Add(new Employee("Maria", "Garcia", 2));
             employeesList.Add(new Employee("Kevin", "Ryder", 3));
-            employeesList.Add(new Employee("Joe", "Smith", 4));
+            employeesList.Add(new Employee("Joe", "Love", 4));
             employeesList.Add(new Employee("Bill", "Hunt", 5));
             employeesList.Add(new Employee("Shawn", "Levi", 6));
             employeesList.Add(new Employee("Jenny", "Bogner", 7));
             employeesList.Add(new Employee("Chang", "Rue", 8));
-            employeesList.Add(new Employee("Joe", "Smith", 9));
+            employeesList.Add(new Employee("Joe", "Binst", 9));
             employeesList.Add(new Employee("Tim", "Turner", 10));
             employeesList.Add(new Employee("Nelly", "Fyiner", 11));
             employeesList.Add(new Employee("Jesse", "Smith", 12));
 
             //foreach loop to create new list of employees
             //with first name "Joe"
-            foreach (Employee employee in employeesList)
+            //List<Employee> joesList = new List<Employee>();
+            //foreach (Employee employee in employeesList)
+            //{
+            //    //checking to see if current employee has joe first name
+            //    if (employee.firstName == "Joe")
+            //    {
+            //        //adding joe employee to joesList
+            //        joesList.Add(employee);
+            //    }
+            //}
+            ////foreach loop to iterate through joesList and print them to the console
+            //foreach (Employee employee in joesList)
+            //{
+            //    Console.WriteLine(employee.Id + " " + employee.firstName + " " + employee.lastName);
+            //}
+
+            //using LAMBDA expressions to create same joesList
+            //and do same task as code above, just shorter
+            List<Employee> joesList = employeesList.Where(x => x.firstName == "Joe").ToList();
+
+            //foreach loop to iterate through each employee
+            //the print them out to the console
+            foreach (Employee employee in joesList)
             {
-                if (employee.firstName == "Joe")
-                {
-                    Console.WriteLine(employee);
-                }
+                Console.WriteLine(employee.firstName + " " + employee.lastName + " " + employee.Id);
             }
+
+
+            //LAMBDA expression that makes list of employees
+            //with an Id number greater than 5
+            List<Employee> aboveFive = employeesList.Where(x => x.Id > 5).ToList();
+
+            foreach (Employee employee in aboveFive)
+            {
+                Console.WriteLine(employee.firstName + " " + employee.lastName + " " + employee.Id);
+            }
+
             Console.ReadLine();
         }
     }
