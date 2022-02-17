7dd0c79e234a8bbd6c0e68c8069800425f359843
DevXerxes <87164130+DevXerxes@users.noreply.github.com>
2022-02-16 19:43:01 -0800

    Logging A Users input
    Includes:
        -receiving an integer input from a user
        -logging that input into a text file using System.IO
        -using ReadAllText to print back the text file to the user

---
+++ InputToLog/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//input the namespace needed to transfer text to files (logging)
+using System.IO;
+
+namespace InputToLog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //asking user for a number
+            Console.WriteLine("Can you enter any number?");
+            //logging the users number to a text file using the syntax below
+            int urNum = Convert.ToInt32(Console.ReadLine());
+            File.WriteAllText(@"C:\Users\vidin\Logs\number.txt", urNum.ToString());
+            //raeding and printing text file back to the user
+            string urNum2 = File.ReadAllText(@"C:\Users\vidin\Logs\number.txt");
+            Console.WriteLine(urNum2);
+            Console.ReadLine();
+
+
+        }
+    }
+}
