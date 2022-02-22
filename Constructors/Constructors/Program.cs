21cd12c2b02c47c386cc90fc550491b701fbc5be
DevXerxes <87164130+DevXerxes@users.noreply.github.com>
2022-02-21 23:34:37 -0800

    A Constructor chaining example.

---
+++ Constructors/Constructors/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //created a const variable so that its value may never be changed
+            //by others than the creator
+            const string appName = "Example of a constant variable.";
+
+            //creating a variable using the keyword "var"
+            var myName = "My name is Marlon";
+            Console.WriteLine(appName + myName);
+            Console.ReadLine();
+
+            //instantiating the 2nd constructor
+            constExample Cost = new constExample();
+        }
+    }
+}
---
+++ Constructors/Constructors/Sample.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    public class constExample
+    {
+        public constExample(): this(22)
+        {
+            //No parameter constructor method.// 1st
+        }
+
+        public constExample(int Age)
+        {
+            //Constructor with one parameter.// 2nd
+        }
+    }
+}
